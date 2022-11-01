5f86ce08d2b1596b08d87d424bfc0d83e5ced73f
Yudin Pavel <yudin.p.s@gmail.com>
2022-11-01 12:09:15 +0700

    feat: add solution of task 10

---
+++ ex10/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(100,1000);
+Console.WriteLine($"Сгенерировано число {number}");
+
+int number2 = (number / 10) % 10;
+
+Console.WriteLine($"Вторая цифра сгенерированного числа {number2}");
