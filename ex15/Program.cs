9c27d9c1bf93145ce86ab19fcda600c0c3c36a32
Yudin Pavel <yudin.p.s@gmail.com>
2022-11-02 16:22:31 +0700

    feat: add solution of task 15

---
+++ ex15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число от 1 до 7 ");
+int day = Convert.ToInt32 (Console.ReadLine());
+
+if (day >= 1 && day <= 5)
+    Console.WriteLine("Не является выходным");
+else if (day == 6 || day == 7)
+        Console.WriteLine("Является выходным");
+    else
+        Console.WriteLine("Вы ввели некорректное число");
