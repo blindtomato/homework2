bd26905968ca1fd9d9e3f9cf37a5deba6e9f4542
Yudin Pavel <yudin.p.s@gmail.com>
2022-11-02 16:07:35 +0700

    feat: add solution of task 13

---
+++ ex13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+if (number / 100 == 0)
+    Console.WriteLine("Третьей цифры нет ");
+else 
+{
+    int k = number;
+    int n3 = 0;
+    while (k >= 100)
+    {
+        n3 = k % 10;
+        k = k / 10;        
+    }
+Console.WriteLine($"Третья цифра заданного числа {n3}");
+}
